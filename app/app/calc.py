659244a662c511e02746683aac989b5b4c3c2460
diwasht007 <diwashtiwari007@gmail.com>
2024-03-01 21:33:32 +1100

    testting wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,11 @@
     calculator function
 """
     
-def add(x,y):
+def add(x, y):
     """Add x & y then return result."""
     return x + y
+
+def subtract(x, y):
+    """Subtract x & y then return result."""
+    return x - y
+
--- app/app/test.py
+++ app/app/test.py
@@ -8,9 +8,16 @@ from django.test import SimpleTestCase
 from app import calc
 
 class calcTest(SimpleTestCase):
+    
     """Test calc module"""
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         
         self.assertEqual(res,11)
+    
+    def test_subtract_numbers(self):
+        "Test subtracting numbers."
+        res = calc.subtract(12, 10)
+        
+        self.assertEqual(res,2)
     
